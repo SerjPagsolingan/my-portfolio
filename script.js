be013af129b5640d7c62b7d7d074ae16cf2c5f4a
SerjPagsolingan <serj.pagsolingan@gmail.com>
2026-05-18 03:46:41 +0800

    Update files - Mon 05/18/2026  3:46:41.60

---
+++ script.js
@@ -0,0 +1,35 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".navbar a");
+
+
+const observerOptions = {
+  root: null, 
+  rootMargin: "-30% 0px -60% 0px",
+  threshold: 0
+};
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    
+    if (entry.isIntersecting) {
+      const id = entry.target.getAttribute("id");
+
+      
+      navLinks.forEach((link) => {
+        
+        link.classList.remove("active");
+        
+        
+        if (link.getAttribute("href") === `#${id}`) {
+          link.classList.add("active");
+        }
+      });
+    }
+  });
+}, observerOptions);
+
+
+sections.forEach((section) => {
+  observer.observe(section);
+});
